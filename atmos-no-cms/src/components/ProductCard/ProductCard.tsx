924fd0ce894411a012ffdabc78b58fe87299e0b7
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2025-10-07 13:27:18 -0400

    slow product img transition

--- atmos-no-cms/src/components/ProductCard/ProductCard.tsx
+++ atmos-no-cms/src/components/ProductCard/ProductCard.tsx
@@ -13,7 +13,7 @@ export default function ProductCard({ product }: { product: Product }) {
           style={{ transformOrigin: "50% 50%" }}
           initial={{ opacity: 0, scale: 0 }}
           whileInView={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
+          transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1] }}
           viewport={{ once: true, amount: 0.4 }}
         >
           <img className="pc__image" src={product.image} alt={product.name} loading="lazy" />
